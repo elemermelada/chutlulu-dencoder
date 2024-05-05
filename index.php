743a48b8e7a36fe0d2bd67c1790eaefa3e60c532
elemermelada <iiasanmi9@gmail.com>
2024-05-05 17:27:59 +0200

    remove log

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 $encrypt = $_GET["mode"];
 $str = $_POST["input"];
 
-file_put_contents("log.txt", $str);
-
 $command = "./a.out ". $encrypt . " ";
 foreach (mb_str_split($str) as $ch) {
     $command .= mb_ord($ch) . " ";
