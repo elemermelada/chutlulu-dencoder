a5624d10cc4a110096034b9b7b121b181482e8fc
elemermelada <iiasanmi9@gmail.com>
2024-05-05 13:43:13 +0200

    created main.cpp

---
+++ main.cpp
@@ -0,0 +1,46 @@
+﻿#include "string"
+#include "iostream"
+#include "cmath"
+#define _USE_MATH_DEFINES
+#include "math.h"
+#include "codecvt"
+
+using namespace std;
+
+std::wstring_convert<std::codecvt<char16_t, char, std::mbstate_t>, char16_t> convert;
+u16string chutlulu = convert.from_bytes(u8"chutlulu");
+
+static wchar_t dencryptChar(const char16_t charin, const int length, const int i, const bool encrypt = true) {
+	double arg1 = cos(((length - (i + 1)) * M_PI));
+	double arg2 = round(log10(pow(i + 1, 4.0)));
+	int arg3 = chutlulu[i % 8] * 69;
+
+	int result = (int)charin;
+	if (encrypt)
+		result += (int)((arg1 * 3 + arg2 + arg3));
+	else
+		result -= (int)((arg1 * 3 + arg2 + arg3));
+
+	result = result % 1033;
+	if (result < 0) result += 1033;
+	return (wchar_t)result;
+}
+
+static void dencryptText(const u16string& input, u16string& output, const bool encrypt = true) {
+	for (int i = 0; i < input.size(); i++) {
+		output += dencryptChar(input[i], input.size(), i, encrypt);
+	}
+}
+
+int main(int argc, char** argv) {
+	char doencrypt = '1';// *(argv[1]);
+	u16string input = convert.from_bytes(u8"Test string");//argv[2]);
+	u16string output;
+
+	dencryptText(input, output, doencrypt == '1');
+
+	for (auto ch : output) {
+		cout << (int)ch << endl;
+	}
+	return 0;
+}
