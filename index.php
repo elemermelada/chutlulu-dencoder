9d195c46593a2963377a2bc64d2a97d33fb4f540
elemermelada <iiasanmi9@gmail.com>
2024-05-05 15:07:11 +0200

    pipe using GET

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-$encrypt = "1";
-$command = "./a.out ". $encrypt . " ";
+$encrypt = $_GET["mode"];
+$str = $_GET["input"];
 
-$str = "ABC";
+$command = "./a.out ". $encrypt . " ";
 foreach (mb_str_split($str) as $ch) {
     $command .= mb_ord($ch) . " ";
 } 
